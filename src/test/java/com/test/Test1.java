d50f9c5b65df6fbce820b5946cb420f836aa4cab
Troy Brand <troy.brand@eviden.com>
2025-03-13 09:47:39 +0100

    Add files

--- src/test/java/com/test/Test1.java
+++ src/test/java/com/test/Test1.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 //import org.junit.Test.*;
 
-
 public class Test1 {
 
 	@Test
@@ -14,13 +13,31 @@ public class Test1 {
 		String name = "Troy";
 		String name1 = "Troy";
 		
+		assertEquals(name,name1);
 		
-		
-		
-		System.out.println("Hello");
-		System.out.println("Funkibon");
+		System.out.println("Test Name");
 	}
 	
+	@Test
+	public void testCMP()
+	{
+		String c1 = "Eviden";
+		String c2 = "Eviden";
+		
+		assertEquals(c1,c2);
+		
+		System.out.println("testCMP");
+	}
 	
+	@Test
+	public void testLocation()
+	{
+		String l1 = "Spain";
+		String l2 = "Spain";
+		
+		assertEquals(l1,l2);
+		
+		System.out.println("Test Location");
+	}
 	
 }
