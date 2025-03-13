90a24260cb1cef7029c6fa7a1dae83b29ae7111b
Troy Brand <troy.brand@eviden.com>
2025-03-13 09:35:59 +0100

    Add files

---
+++ src/test/java/com/test/Test1.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+//import org.junit.Test.*;
+
+
+public class Test1 {
+
+	@Test
+	public void testName()
+	{
+		String name = "Troy";
+		String name1 = "Troy";
+		
+		
+		
+		
+		System.out.println("Hello");
+		System.out.println("Funkibon");
+	}
+	
+	
+	
+}
